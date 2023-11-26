66737b669da2075d262834812f1c51760100e225
Pews Lee <978221138@qq.com>
2023-11-26 16:08:00 +0800

    Version 1.1 of Clipboard application.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"ClipBoard/static"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	uuid2 "github.com/google/uuid"
 	"log"
 	"net"
 	"net/http"
@@ -33,25 +32,25 @@ func main() {
 
 	r.NoRoute(gin.WrapH(http.FileServer(http.FS(web.Static))))
 
-	r.POST("/add", func(c *gin.Context) {
+	r.POST("/add/:token", func(c *gin.Context) {
+		tokenStr := c.Param("token")
 		var message message
-		uuid := uuid2.NewString()
 		if err := c.BindJSON(&message); err != nil {
 			log.Fatalln("Read in went wrong..")
 		} else {
-			messageMap[uuid] = message.Clipboard
+			messageMap[tokenStr] = message.Clipboard
 		}
 
-		URL := "http://" + localAddr + ":8080/get/" + uuid
+		URL := "http://" + localAddr + ":8080/get/" + tokenStr
 
 		c.JSON(200, gin.H{
 			"url": URL,
 		})
 	})
 
-	r.GET("get/:uuid", func(c *gin.Context) {
-		uuid := c.Param("uuid")
-		c.String(200, messageMap[uuid])
+	r.GET("get/:token", func(c *gin.Context) {
+		tokenStr := c.Param("token")
+		c.String(200, messageMap[tokenStr])
 	})
 
 	r.Run(":8080") // 监听并在 0.0.0.0:8080 上启动服务
