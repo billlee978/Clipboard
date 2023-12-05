0552ebd6b7794f2ef56d9bb9f75a618bacdee78e
Pews Lee <978221138@qq.com>
2023-12-05 16:34:51 +0800

    Reduce front end files size.

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net"
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 )
 
 type message struct {
@@ -17,17 +15,6 @@ type message struct {
 }
 
 var messageMap = make(map[string]string)
-var localAddr string
-
-func init() {
-	var ips []string = GetIps()
-	for _, ip := range ips {
-		if strings.HasPrefix(ip, "172") {
-			localAddr = ip
-			break
-		}
-	}
-}
 
 func main() {
 	r := gin.Default()
@@ -110,7 +97,7 @@ func main() {
 			messageMap[tokenStr] = message.Clipboard
 		}
 
-		URL := "http://" + localAddr + ":8080/get/" + tokenStr
+		URL := "https://pews.top/get/" + tokenStr
 
 		c.JSON(200, gin.H{
 			"url": URL,
@@ -123,25 +110,6 @@ func main() {
 	})
 
 	r.Run(":8080") // 监听并在 0.0.0.0:8080 上启动服务
-}
-
-func GetIps() (ips []string) {
-	interfaceAddr, err := net.InterfaceAddrs()
-	if err != nil {
-		fmt.Printf("fail to get net interfaces ipAddress: %v\n", err)
-		return ips
-	}
-
-	for _, address := range interfaceAddr {
-		ipNet, isVailIpNet := address.(*net.IPNet)
-		// 检查ip地址判断是否回环地址
-		if isVailIpNet && !ipNet.IP.IsLoopback() {
-			if ipNet.IP.To4() != nil {
-				ips = append(ips, ipNet.IP.String())
-			}
-		}
-	}
-	return ips
 }
 
 func createTokenFolder(folderPath string) bool {
